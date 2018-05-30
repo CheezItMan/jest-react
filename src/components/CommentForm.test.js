031eeba491bf363337d78951b15a151269677664
CheezItMan <chris@adadevelopersacademy.org>
2018-05-30 14:51:36 -0700

    1st test on NewCommentform

--- src/components/CommentForm.test.js
+++ src/components/CommentForm.test.js
@@ -1,7 +1,13 @@
+import React from 'react';
 import CommentForm from 'components/CommentForm';
+import { mount } from 'enzyme';
 
 describe('CommentForm', () => {
-  it('Can be rendered', () => {
+  it('has a text area and a button', () => {
+    const wrapped = mount(<CommentForm />);
+
+    expect(wrapped.find('textarea').length).toEqual(1);
+    expect(wrapped.find('input[type="submit"]').length).toEqual(1);
 
 
   });
